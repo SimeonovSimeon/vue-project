9e54d674677a6c3187854615f464623b7f0563d5
Simeon <montyto@gmail.com>
2020-05-03 18:07:10 +0100

    create / import new component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Todo Item</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem"
+}
+</script>
+
+<style scoped>
+.is-complete {
+     text-decoration: line-through;
+}
+</style>
