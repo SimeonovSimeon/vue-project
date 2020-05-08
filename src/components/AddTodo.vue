5df41ef891235df208dc07e218c46b3241478d56
Simeon <montyto@gmail.com>
2020-05-08 11:57:02 +0100

    adding an item

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -8,7 +8,8 @@
 </template>
 
 <script>
-import uuid from 'uuid';
+//import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 export default {
     name: "AddTodo",
     data() {
@@ -17,14 +18,17 @@ export default {
         }
     },
     methods: {
-        addTodo(){
+        addTodo(e){
+            e.preventDefault();
             const newTodo = {
-                id: uuid.v4(),
+                id: uuidv4(),
                 title: this.title,
                 completed: false
             }
             //send up to parent
             this.$emit('add-todo', newTodo);
+
+            this.title = '';
         }
     }
 }
